06c37f7867ab585e50b5281fd71461c3d697367b
Yechiel Wiesel <ywiesel@nvidia.com>
2023-12-18 03:53:08 +0200

    update

---
+++ src/AnalyzeType.tsx
@@ -0,0 +1,34 @@
+import { RefObject, useState } from "react"
+import { Section, SectionTypes } from "./Results"
+import ReactPlayer from "react-player"
+
+type Params = {
+    sections: Section[]
+    SectionType: SectionTypes
+    playerRef: RefObject<ReactPlayer>
+    setPlaying: any
+}
+
+export default ({ SectionType, sections, playerRef, setPlaying }: Params) => {
+    const [threshold, setThreshold] = useState(5)
+    const videoDuration = playerRef.current?.getDuration() || 0
+    return <div style={{position: 'relative', height: 60}}>
+        <br/><br/>
+        <div className="progress">
+          <div className="progress-bar progress-bar-striped bg-success" role="progressbar" style={{width: 0}} >
+                {sections
+                .filter(section => section.score < threshold)
+                .map(section => {
+                    const location = section.start / videoDuration * 100
+                    return <div style={{position: 'absolute', borderRadius: '50%', width: 20, height: 20, background: 'pink', bottom: -5, left:  location + '%', cursor: 'pointer'}} onClick={() => {
+                        playerRef.current?.seekTo(section.start)
+                        setPlaying(false)
+                        }}></div>
+                })}
+          </div>
+       </div>
+        <p style={{position: 'absolute', left: '-7em', bottom: '-1em', color: 'white', fontSize: 25}}>{SectionType}</p>
+        <label htmlFor="customRange3" className="form-label" style={{position: 'absolute', 'width': '10vh', right: -200, bottom: 0}}>{threshold}</label>
+        <input type="range" className="form-range" min="1" max="10" step="1" id="customRange3" value={threshold} onChange={e => setThreshold(e.target.valueAsNumber)} style={{position: 'absolute', 'width': '10vh', right: -150, bottom: -10, color: 'blue'}} />
+      </div>
+}
